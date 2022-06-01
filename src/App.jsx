4485cdf3e86ce2ea9b2c5bfd6b4fef6eae2d9cb4
Gustavo <gustavo.prog40@gmail.com>
2022-06-01 09:46:35 -0300

    fazendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
 import Header from './components/Header'
+import { ContainerName, Image, H1, Text, ImageContainer } from './styles-app'
+import Foto from './assets/image1.jpg'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <ContainerName>
+        <H1>Gerusa Gamm - Tradutora Juramentada</H1>
+      </ContainerName>
+      <ImageContainer>
+        <Text>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
+          of classical Latin literature from 45 BC, making it over 2000 years old. Richard
+          McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the
+          more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the
+          cites of th
+        </Text>
+        <Image src={Foto} alt="imagem" />
+      </ImageContainer>
     </div>
   )
 }
---
+++ src/styles-app.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+export const ContainerName = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10.5rem;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: space-around;
+  background: #ffffcc;
+
+  @media screen and (max-width: 700px) {
+    width: 100%;
+  }
+`
+
+export const H1 = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 45%;
+
+  @media screen and (max-width: 700px) {
+    font-size: 1.3rem;
+    width: 70%;
+  }
+`
+
+export const ImageContainer = styled.div`
+  display: flex;
+  width: 400;
+  height: 400px;
+  flex-direction: row;
+  margin-top: 100px;
+
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    height: auto;
+    padding-bottom: 260px;
+    margin-top: 28px;
+  }
+`
+export const Image = styled.img`
+  width: 25%;
+  height: 70%;
+  border-radius: 48%;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    border-radius: 48%;
+    width: 48%;
+    margin-top: 40px;
+    height: 150px;
+  }
+`
+export const Text = styled.div`
+  width: 38%;
+  color: darkblue;
+  font-family: 'Blinker';
+  font-size: 30px;
+  margin-right: 115px;
+
+  @media screen and (max-width: 800px) {
+    font-size: 19px;
+    width: 88%;
+    height: auto;
+    margin-left: 90px;
+    margin-top: 20px;
+    margin-right: 78px;
+  }
+`
